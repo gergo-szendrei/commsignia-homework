3fbe68e92667e6a857a6e309c9aa090bef64b03f
“gergo-szendrei” <szendrei.gergo94@gmail.com>
2024-04-23 09:42:59 +0200

    Vehicle package logging enhancement

--- backend/src/main/java/com/commsignia/backend/vehicle/controller/VehicleController.java
+++ backend/src/main/java/com/commsignia/backend/vehicle/controller/VehicleController.java
@@ -4,6 +4,8 @@ import com.commsignia.backend.vehicle.dto.Position;
 import com.commsignia.backend.vehicle.dto.RegisterResponse;
 import com.commsignia.backend.vehicle.dto.VehiclesResponse;
 import com.commsignia.backend.vehicle.service.VehicleService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +19,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class VehicleController {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(VehicleController.class);
+
 	private final VehicleService vehicleService;
 
 	public VehicleController(VehicleService vehicleService) {
@@ -28,11 +32,14 @@ public class VehicleController {
 			@RequestParam(value = "latitude") Double latitude,
 			@RequestParam(value = "longitude") Double longitude,
 			@RequestParam(value = "radius") Long radius) {
+		LOGGER.info("Get vehicles in radius controller called with latitude {}, longitude {}, radius {}",
+				latitude, longitude, radius);
 		return ResponseEntity.ok(vehicleService.getVehiclesInRadius(latitude, longitude, radius));
 	}
 
 	@PostMapping("vehicles")
 	public ResponseEntity<RegisterResponse> registerVehicle() {
+		LOGGER.info("Register vehicle controller called");
 		return ResponseEntity.ok(vehicleService.registerVehicle());
 	}
 
@@ -40,6 +47,7 @@ public class VehicleController {
 	public ResponseEntity<Void> updatePosition(
 			@PathVariable("id") Long id,
 			@RequestBody Position position) {
+		LOGGER.info("Update position controller called with id {}, position {}", id, position);
 		vehicleService.updatePosition(id, position);
 		return ResponseEntity.ok().build();
 	}
--- backend/src/main/java/com/commsignia/backend/vehicle/service/VehicleServiceImpl.java
+++ backend/src/main/java/com/commsignia/backend/vehicle/service/VehicleServiceImpl.java
@@ -10,12 +10,17 @@ import com.commsignia.backend.vehicle.dto.Vehicle;
 import com.commsignia.backend.vehicle.dto.VehiclesResponse;
 import com.commsignia.backend.vehicle.entity.VehicleEntity;
 import com.commsignia.backend.vehicle.repository.VehicleRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
+// Can rely on auto-created transaction and omit the annotation, but it is a good practice to follow the annotation approach
 @Transactional
 public class VehicleServiceImpl implements VehicleService {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(VehicleServiceImpl.class);
 
 	private final VehicleRepository vehicleRepository;
 
@@ -25,6 +30,8 @@ public class VehicleServiceImpl implements VehicleService {
 
 	@Override
 	public VehiclesResponse getVehiclesInRadius(Double latitude, Double longitude, Long radius) {
+		LOGGER.info("Get vehicles in radius service called with latitude {}, longitude {}, radius {}",
+				latitude, longitude, radius);
 		List<Vehicle> vehicles = vehicleRepository.getVehiclesInRadius(latitude, longitude, radius).stream()
 				.map(Vehicle::toVehicle)
 				.toList();
@@ -33,9 +40,13 @@ public class VehicleServiceImpl implements VehicleService {
 
 	@Override
 	public RegisterResponse registerVehicle() {
+		LOGGER.info("Register vehicle service called");
 		VehicleEntity vehicleEntity = new VehicleEntity();
+
+		LOGGER.info("Saving vehicle: {}", vehicleEntity);
 		// Assigning it back does not needed for proper operation, but helps testability
 		vehicleEntity = vehicleRepository.save(vehicleEntity);
+
 		return RegisterResponse.builder()
 				.id(vehicleEntity.getId())
 				.build();
@@ -43,13 +54,17 @@ public class VehicleServiceImpl implements VehicleService {
 
 	@Override
 	public void updatePosition(Long id, Position position) {
+		LOGGER.info("Update position service called with id {}, position {}", id, position);
 		Optional<VehicleEntity> optional = vehicleRepository.findById(id);
 		if (optional.isEmpty()) {
 			throw new CustomRuntimeException(String.format("Cannot update position. Vehicle by id %1$s cannot be found.", id));
 		}
+
 		VehicleEntity vehicleEntity = optional.get();
 		vehicleEntity.setLatitude(position.getLatitude());
 		vehicleEntity.setLongitude(position.getLongitude());
+
+		LOGGER.info("Saving position: {}", vehicleEntity);
 		// It would save even without calling save explicitly, but this approach helps testability
 		vehicleRepository.save(vehicleEntity);
 	}
